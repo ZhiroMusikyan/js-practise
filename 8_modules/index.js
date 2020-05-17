db9ce4e98468b126c876e3828f6a57a0c586315e
zhirayr <jiromusikyan@gmail.com>
2020-05-17 22:51:06 +0400

    practise with import

--- 8_modules/index.js
+++ 8_modules/index.js
@@ -1,7 +1,15 @@
-import loger, {COLOR, compute} from "./module";
+// import
+import {one, two} from './module.js'
+import loger, {COLOR, compute} from "./module.js";
+
+//rename import
+import {one as num1, two as num2} from './module';
+
+//one import
+import * as numbers from './module.js';
 
 console.log(COLOR);
 let res = compute(1, 2);
 console.log(res);
 
-loger.log()
+loger.log();
--- 8_modules/module.js
+++ 8_modules/module.js
@@ -1,3 +1,29 @@
+// export variables
+export let one = 1;
+let two = 2;
+let three = 3;
+// export {two};
+export {two, three};
+
+//export class..
+export class Person {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+//export function..
+export function sayHello() {
+    console.log('hello');
+}
+
+// or
+export {Person, sayHello};
+
+//rename exporting variable
+
+export {one as Once, two as twice};
+
 const privateVariable = 2;
 
 export const COLOR = '*bababa';
